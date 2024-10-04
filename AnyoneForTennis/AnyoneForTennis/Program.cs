6ea7925e02d5ed3682db405570b282451e2a609e
s350764 <hoangfantastic11@gmail.com>
2024-10-04 13:46:42 +0930

    Fixxed dbcontext

--- AnyoneForTennis/AnyoneForTennis/Program.cs
+++ AnyoneForTennis/AnyoneForTennis/Program.cs
@@ -1,6 +1,7 @@
 using AnyoneForTennis.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AnyoneForTennis.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
